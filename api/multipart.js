c7c735dadddb1f4b38453a64c3679165d09a445b
amarnesskind <kumaramar1997@gmail.com>
2020-01-28 23:40:44 +0530

    created fs to save files

--- api/multipart.js
+++ api/multipart.js
@@ -2,12 +2,30 @@ var express = require('express')
 var app = express()
 var multer = require('multer')
 var upload = multer()
+var fs = require('fs');
 var port = 3001
 
 app.post('/profile', upload.none(), (req, res) => {
         const formData = req.body
         console.log('form Data', formData)
-        res.status(200).send(formData)
+        const reqObj = {
+            "payload" : formData,
+            "headers" : req.headers
+        }
+        fs.writeFile('./../file/formdata.txt', JSON.stringify(reqObj),(err) => {
+           if(err){
+               res.status(500).send('something went wrong')
+           } else {
+            fs.readFile('./../file/formdata.txt',(err,data) => {
+                if(err){
+                 res.status(500).send('something went wrong') 
+                } else {
+                 res.status(200).send(JSON.parse(data))
+                }
+            })
+           }
+        })      
+        console.log('form Data', req.headers)
 })
 app.get('/',(req, res) => res.status(200).send('all is good'))
 
